7cde8fecbfcae927b1d698311b1a01567f4ed83a
ArmandoArV <armando.arredondo.valle@gmail.com>
2022-12-15 13:54:05 -0600

    Added flexbox container.

--- tecxotic/src/Components/PilotContainer/PilotContainer.js
+++ tecxotic/src/Components/PilotContainer/PilotContainer.js
@@ -3,8 +3,28 @@ import styles from "./PilotContainer.css";
 
 export default function PilotContainer() {
     return (
-        <div className="PilotCards-wrapper">
+        <div className="PilotCards-container">
+            <div className="PilotCard-Left">
+                <h1>Left</h1>
+            </div>
+            <div className="PilotCard-Right">
+                <h1>Right</h1>
+                <h1>Right</h1>
 
+                <h1>Right</h1>
+
+                <h1>Right</h1>
+
+                <h1>Right</h1>
+
+                <h1>Right</h1>
+
+                <h1>Right</h1>
+
+                <h1>Right</h1>
+
+
+            </div>
         </div>
     );
 

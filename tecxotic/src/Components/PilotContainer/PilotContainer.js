334983679e4fb51c8d6b53652458b2a3ab64f67e
DulceBucio <114613877+DulceBucio@users.noreply.github.com>
2022-12-15 14:06:33 -0600

    ;_;

--- tecxotic/src/Components/PilotContainer/PilotContainer.js
+++ tecxotic/src/Components/PilotContainer/PilotContainer.js
@@ -3,28 +3,8 @@ import styles from "./PilotContainer.css";
 
 export default function PilotContainer() {
     return (
-        <div className="PilotCards-container">
-            <div className="PilotCard-Left">
-                <h1>Left</h1>
-            </div>
-            <div className="PilotCard-Right">
-                <h1>Right</h1>
-                <h1>Right</h1>
-
-                <h1>Right</h1>
-
-                <h1>Right</h1>
-
-                <h1>Right</h1>
-
-                <h1>Right</h1>
-
-                <h1>Right</h1>
-
-                <h1>Right</h1>
-
-
-            </div>
+        <div className="PilotCards-wrapper">
+            
         </div>
     );
 

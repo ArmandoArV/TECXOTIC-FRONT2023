15e36b928b6a1e920b043ad6f51f2cae18f353a9
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-01-17 20:26:22 -0600

    Removed 3d view.

--- tecxotic/src/Components/PilotContainer/PilotContainer.js
+++ tecxotic/src/Components/PilotContainer/PilotContainer.js
@@ -4,9 +4,6 @@ import CameraProp from "../CameraProp/CameraProp";
 import IndicatorsContainer from "../IndicatorsContainer/IndicatorsContainer";
 import SpeedContainer from "../SpeedContainer/SpeedContainer";
 import Visualizer from "../VisualizerComponent/Visualizer";
-import ROV from "../../Assets/cubo.obj";
-import { Canvas, useLoader } from '@react-three/fiber';
-import {Environment, OrbitControls} from "@react-three/drei";
 export default function PilotContainer() {
     return (
         <div className="PilotCards-container">
@@ -17,15 +14,7 @@ export default function PilotContainer() {
             </div>
             <div className={"PilotCard-Right"}>
                 <h1>Navigation</h1>
-                <Canvas>
-                    <OrbitControls position={[0, 0, 0]} />
-                    <Suspense fallback={null}>
-                        <Environment preset="city" />
-                    </Suspense>
-                    <Suspense fallback={null}>
-                        <Visualizer modelUrl={ROV} />
-                    </Suspense>
-                </Canvas>
+
             </div>
         </div>
     );
--- tecxotic/src/Components/VisualizerComponent/Visualizer.js
+++ tecxotic/src/Components/VisualizerComponent/Visualizer.js
@@ -1,48 +1,16 @@
-import React, {useEffect, useMemo, useRef, useState} from 'react'
-import {useLoader} from '@react-three/fiber'
-import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
-import {useTexture} from "@react-three/drei";
-import Textura from "../../Images/testTexture.jpg";
-export default function(modelUrl) {
-    /*
-    const ref = useRef()
-    const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
-    useFrame((state, delta) => (ref.current.rotation.x += delta))
-   */
-    const obj = useLoader(OBJLoader, modelUrl);
-    const texture = useTexture(Textura);
+import React from "react";
+import styles from "./Visualizer.css";
+import { Canvas } from "react-three-fiber";
 
-    const geometry = useMemo(() =>{
-
-        let g;
-        obj.traverse((child) => {
-            if (child.isMesh) {
-                g = child.geometry;
-
-            }
-
-        });
-        return g;
-
-    }, [obj]);
-
+export default function Visualizer() {
     return (
-        <mesh geometry={geometry} scale={0.04}>
-            <meshPhysicalMaterial map={texture}/>
-        </mesh>
-    )
+        <div className="Visualizer-container">
+            <Canvas>
+                <mesh>
+                    <boxGeometry args={[1, 1, 1]} />
+                    <meshStandardMaterial color="hotpink" />
+                </mesh>
+            </Canvas>
+        </div>
+    );
 }
-
-/*
-        <mesh
-            {...modelUrl}
-            ref={ref}
-            scale={clicked ? 1.5 : 1}
-            onClick={(event) => click(!clicked)}
-            onPointerOver={(event) => hover(true)}
-            onPointerOut={(event) => hover(false)}>
-            <boxBufferGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
- */

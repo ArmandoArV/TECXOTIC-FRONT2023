19a63cf01b921ed12821959bec439600b7ffc284
leo <leoperez3006@gmail.com>
2023-01-23 14:13:58 -0600

    Driving Mode Icon

--- tecxotic/src/Components/NavBar/NavBar.js
+++ tecxotic/src/Components/NavBar/NavBar.js
@@ -9,6 +9,8 @@ import Indicator from "../../Images/Indicator.svg";
 import Microchip from "../../Images/Microchip.svg";
 import Wifi from "../../Images/Wifi.svg";
 import SliderComponent from '../SliderComponent/SliderComponent';
+import DrivingMode from '../../Images/DrivingMode.svg';
+
 
 const Navbar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -21,7 +23,7 @@ const Navbar = (props) => {
             <ol className='Indicators-List'>
                 <li> <IndicatorC image={Wifi} alt="Wifi" className="Wifi-Icon"/> </li>
                 <li> <IndicatorC image={Flag} alt="Flag" className="Fhalag-Icon"/> </li>
-                <li> <IndicatorC image={Microchip} alt="Microchip" className="Microchip-Icon"/> </li>
+                <li> <IndicatorC image={DrivingMode} alt="Driving" className="DrivingMode-Icon"/> </li>
                 <li> <IndicatorC image={Controller} alt="Controller" className="Controller-Icon"/> </li>
                 <li> <IndicatorC image={Indicator} alt="Indicator" className="Indicator-Icon"/> </li>
                 <li className='SpeedContainer'><h2>Speed: {props.speed}</h2></li>

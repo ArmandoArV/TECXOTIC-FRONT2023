6f16f1c92ea0b8a2913ae6ea8dbe4cd85bc23128
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-02-01 09:34:06 -0600

    Added Copilot page

--- tecxotic/src/App.js
+++ tecxotic/src/App.js
@@ -1,7 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Pilot from "./Pages/Pilot/Pilot";
 import PilotContainer from "./Components/PilotContainer/PilotContainer";
 import { useState, useEffect } from "react";
 

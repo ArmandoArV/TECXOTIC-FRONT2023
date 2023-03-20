df9a41eaf6a8ec23d7a443133c1c422f97e7336f
MikeDev0X <mike0vortex@gmail.com>
2023-03-19 21:47:11 -0600

    eDNA array implemented

--- backend/routes/utils/photoToText.py
+++ backend/routes/utils/photoToText.py
@@ -7,12 +7,26 @@ def extractText():
     photoName = "DNA_sequence.png"
     photo = Image.open(photoName)
 
+
     #set pytesseract path
     pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    eDNA_array = []
 
+    # translates image to text and removes spaces
     text = pytesseract.image_to_string(photo)
-    print(text)
+    text = text.replace(' ','') 
 
-    return text
+    # removes blank lines and stores each individual line in eDNA_array
+    for line in text.splitlines():
+        if(line == ''):
+            continue
+        eDNA_array.append(line)
 
-#extractText()
+
+    #print(text)
+    print(eDNA_array)
+
+    #returns array with the 3 coral reef fish eDNA substrings
+    return eDNA_array
+
+extractText()

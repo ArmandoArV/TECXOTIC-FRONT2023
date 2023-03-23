8419499afd4d2309091d4f4f384e2f63007013ea
DulceBucio <114613877+DulceBucio@users.noreply.github.com>
2023-03-22 23:36:47 -0600

    changes

--- tecxotic/src/Components/ComparatorContainer/ComparatorContainer.js
+++ tecxotic/src/Components/ComparatorContainer/ComparatorContainer.js
@@ -1,15 +1,18 @@
 import React,{useState} from 'react';
+import "./ComparatorContainer.css"
 
 export default function ComparatorContainer(){
     const [squares,setSquares] = useState(0);
     return(
         <>
             <div className="comparatorContainer">
-                <div className="topContainer"></div>
-                <div className="topMediumContainer"></div> 
+                <div className="topContainer">
+                </div>
+                <div className="topMediumContainer">
+                    </div> 
                 <div className="bottomContainer"></div>
                 <div className="squaresBox">
-                    <p>Squares recovered {squares}</p>
+                    <p>Squares recovered: {squares}</p>
                 </div>
             </div>
         </>

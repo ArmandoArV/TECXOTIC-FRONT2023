636a16d8d74aaf58dd59fd4c371344560aa6e201
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-03-26 20:17:33 -0600

    ComparatorContainer updated.

--- tecxotic/src/Components/ComparatorContainer/ComparatorContainer.js
+++ tecxotic/src/Components/ComparatorContainer/ComparatorContainer.js
@@ -1,24 +1,23 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import "./ComparatorContainer.css";
 import FileUploadButton from '../FileButtonComponent/FileButtonComponent';
 
-const ComparatorContainer = () => {
+export default  function  ComparatorContainer (){
     const [imageUrls, setImageUrls] = useState([]);
-  
+
     const handleFileUpload = (file) => {
-      const reader = new FileReader();
-  
-      reader.addEventListener('load', () => {
-        setImageUrls([...imageUrls, reader.result]); 
-      });
-  
-      reader.readAsDataURL(file);
+        const reader = new FileReader();
+
+        reader.addEventListener('load', () => {
+            setImageUrls([...imageUrls, reader.result]);
+        });
+
+        reader.readAsDataURL(file);
     };
 
 
-function ComparatorContainer(){
-    const [squares,setSquares] = useState(0);
-    return(
+    const [squares, setSquares] = useState(0);
+    return (
         <>
             <div className="comparatorContainer">
                 <div className="topContainer">
@@ -27,17 +26,14 @@ function ComparatorContainer(){
                     </button>
                 </div>
                 <div className="topMediumContainer">
-                    </div> 
+                </div>
                 <div className="bottomContainer">
-                <FileUploadButton handleFileUpload={handleFileUpload} />
+                    <FileUploadButton handleFileUpload={handleFileUpload} />
                 </div>
                 <div className="squaresBox">
                     <p>Squares recovered: {squares}</p>
                 </div>
             </div>
         </>
-        );
+    );
 }
-}
-
-export default ComparatorContainer;

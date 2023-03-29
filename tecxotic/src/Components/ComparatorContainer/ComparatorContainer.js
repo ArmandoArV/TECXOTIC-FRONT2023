4f7337f650312c8529aa258d0d03136682a30e1b
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-03-29 10:22:44 -0600

    Corrections in the structure of the ComparatorContainer

--- tecxotic/src/Components/ComparatorContainer/ComparatorContainer.js
+++ tecxotic/src/Components/ComparatorContainer/ComparatorContainer.js
@@ -5,14 +5,14 @@ import CameraProp from "../CameraProp/CameraProp";
 export default function ComparatorContainer() {
     const cameras = [
         {
-          image: "http://218.45.5.57:80/SnapshotJPEG?Resolution=640x480&amp;Quality=Clarity&amp;1674619720",
-          idImg: "Main1",
+            image: "http://218.45.5.57:80/SnapshotJPEG?Resolution=640x480&amp;Quality=Clarity&amp;1674619720",
+            idImg: "Main1",
         },
         {
-          image: "http://158.58.130.148/mjpg/video.mjpg",
-          idImg: "Main1",
+            image: "http://158.58.130.148/mjpg/video.mjpg",
+            idImg: "Main1",
         }
-      ];
+    ];
 
 
     const [squares, setSquares] = useState(0);
@@ -20,33 +20,54 @@ export default function ComparatorContainer() {
         <>
             <div className="comparatorContainer">
                 <div className="topContainer">
-                    <button className="taskButton">
-                        Start Monitoring
-                    </button>
+                    <div className="taskStartContainer">
+                        <button className="taskButton">
+                            Start Monitoring
+                        </button>
+                    </div>
                 </div>
                 <div className="topMediumContainer">
                     <div className="photoContainerLeft">
-                        <CameraProp
-                            image={cameras[0].image}
-                            idImg={cameras[0].idImg}
-                            styles={{ width: "95%", height: "95%", borderRadius: "25px" }}
-                        />
+                        <div className="photoContainerLeftTop">
+                            <div className="Frame">
+                                <CameraProp
+                                    image={cameras[0].image}
+                                    idImg={cameras[0].idImg}
+                                    styles={{ width: "95%", height: "90%", borderRadius: "20px", padding: "15px"}}
+                                />
+                            </div>
+                        </div>
+                        <div className="photoContainerLeftBottom">
+                            <div className="ButtonCompareContainer">
+                            </div>
+                        </div>
                     </div>
                     <div className="photoContainerRight">
-                        <CameraProp
-                            image={cameras[0].image}
-                            idImg={cameras[0].idImg}
-                            styles={{ width: "95%", height: "95%", borderRadius: "25px" }}
-                        />
+                        <div className="photoContainerRightTop">
+                            <div className="Frame">
+                                <CameraProp
+                                    image={cameras[1].image}
+                                    idImg={cameras[1].idImg}
+                                    styles={{ width: "95%", height: "90%", borderRadius: "20px", padding: "15px" }}
+                                />
+                            </div>
+                        </div>
+
+                        <div className="photoContainerRightBottom">
+                            <div className="ButtonCompareContainer">
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div className="buttonContainer" >
-                    <div className="leftButtonContainer"></div>
-                    <div className="rightButtonContainer"></div>
-                </div>
+
                 <div className="bottomContainer">
                     <div className="squaresBox">
-                        <p>Squares recovered: {squares}</p>
+                        <div className="squaresText">
+                            <p>Number of squares: </p>
+                        </div>
+                        <div className="squaresNumber">
+                            <p> {squares}</p>
+                        </div>
                     </div>
                 </div>
             </div>

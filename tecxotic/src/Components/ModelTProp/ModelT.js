d82b4a95070f8e2c76f357564113b680e7e385f4
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-04-02 21:52:29 -0600

    3DModelContainer

--- tecxotic/src/Components/ModelTProp/ModelT.js
+++ tecxotic/src/Components/ModelTProp/ModelT.js
@@ -4,8 +4,51 @@ import "./ModelT.css";
 export default function ModelT() {
     return (
         <>
-            <div className="modelT">
-                <h1>Model T</h1>
+            <div className="modelTContainer">
+                <div className="ModelTopContainer">
+                    <div className="CanvasContainer">
+                        <canvas className="ModelCanvas" width="500" height="500"></canvas>
+                    </div>
+                </div>
+                <div className="ModelBottomContainer">
+                    <div className="ModelBoxContainer">
+                        <div className="ModelBox">
+                            <div className="ModelboxTextContainer">
+                                <h1 className="ModelboxText">
+                                    Width
+                                </h1>
+                            </div>
+                            <div className="ModelboxInputContainer">
+                                <input className="ModelboxInput" type="number" placeholder="Width" min={0} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ModelBoxContainer">
+                        <div className="ModelBox">
+                            <div className="ModelboxTextContainer">
+                                <h1 className="ModelboxText">
+                                    Height
+                                </h1>
+                            </div>
+                            <div className="ModelboxInputContainer">
+                                <input className="ModelboxInput" type="number" placeholder="Height" min={0} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="ModelBoxContainer">
+                        <div className="ModelBox">
+                            <div className="ModelboxTextContainer">
+                                <h1 className="ModelboxText">
+                                    Length
+                                </h1>
+                            </div>
+                            <div className="ModelboxInputContainer">
+                                <input className="ModelboxInput" type="number" placeholder="Length" min={0} />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </>
     );

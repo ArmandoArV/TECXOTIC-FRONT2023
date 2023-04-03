c140e4c4973f6cb3408c97378811067d02385f0e
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-04-02 18:37:33 -0600

    MeasureComponent  structure and styles created.

--- tecxotic/src/Components/MeasureProp/Measure.js
+++ tecxotic/src/Components/MeasureProp/Measure.js
@@ -1,12 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Measure.css";
 
 export default function Measure() {
+    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);
+    const [length, setLength] = useState(0);
+    const [startMeasure, setStartMeasure] = useState(false);
+
     return (
         <div className="measure-container">
-            <div className="measure">
-                <div className="start-measure">
-                    <button className="start-measure-button">Start Measure</button>
+            <div className="startMeasureContainer">
+                <button className="startMeasureButton">Start Measure</button>
+            </div>
+            <div className="MeasuresContainer">
+                <div className="MedidasContainer">
+                    <div className="MeasureContainer">
+                        <div className="MeasureBox">
+                            <div className="MeasureTextContainer">
+                                <h1 className="MeasureText">
+                                    Width
+                                </h1>
+                            </div>
+                            <div className="MeasureValueContainer">
+                                <h2 className="MeasureValue">
+                                    {width}
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="MeasureContainer">
+                        <div className="MeasureBox">
+                            <div className="MeasureTextContainer">
+                                <h1 className="MeasureText">
+                                    Height
+                                </h1>
+                            </div>
+                            <div className="MeasureValueContainer">
+                                <h2 className="MeasureValue">
+                                    {height}
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="MeasureContainer">
+                        <div className="MeasureBox">
+                            <div className="MeasureTextContainer">
+                                <h1 className="MeasureText">
+                                    Length
+                                </h1>
+                            </div>
+                            <div className="MeasureValueContainer">
+                                <h2 className="MeasureValue">
+                                    {length}
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

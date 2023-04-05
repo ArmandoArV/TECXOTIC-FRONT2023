d29ed20b6fadcf9fd22968c9663e935b28e6ff50
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-04-05 15:58:41 -0600

    Pilot and Constatns files updated.

--- tecxotic/src/Constants.js
+++ tecxotic/src/Constants.js
@@ -13,3 +13,4 @@ export const arrayOfCameras = [
         idImg: "Main1",
       }
 ];
+
--- tecxotic/src/Pages/Pilot/Pilot.js
+++ tecxotic/src/Pages/Pilot/Pilot.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import PilotContainer from "../../Components/PilotContainer/PilotContainer";
+import { connectionStatus } from "../../Constants";
+
 
 
 export default function PilotPage() {
+    const [wifiStatus, setWifiStatus] = useState(true);
+    const [gamepadStatus, setGamepadStatus] = useState(true);
+    const [flagStatus, setFlagStatus] = useState(false);
+    const [gearStatus, setGearStatus] = useState(true);
+    const [rotation, setRotation] = useState(0);
+    const [pitch, setPitch] = useState(0);
+    const [yaw, setYaw] = useState(90);
+
     return (
         <>
-            <PilotContainer wifiStatus={true} gamepadStatus={true} flagStatus={false} gearStatus={true} rotation={0} pitch={0} yaw={90} />
+            <PilotContainer wifiStatus={setWifiStatus} gamepadStatus={setGamepadStatus} flagStatus={setFlagStatus} gearStatus={setGearStatus} rotation={setRotation} pitch={setPitch} yaw={setYaw} />
         </>
     );
 }

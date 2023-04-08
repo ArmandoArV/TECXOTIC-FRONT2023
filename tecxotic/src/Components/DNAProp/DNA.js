ee2989aeb549b847c24655c238ea937bd45a3bd0
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-04-08 15:13:46 -0600

    DNA Interface added

--- tecxotic/src/Components/DNAProp/DNA.js
+++ tecxotic/src/Components/DNAProp/DNA.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import FileUploader from "../UploadFileButton/UploadFile";
+
+
 
 export default function DNA() {
     return (
         <>
-            <div className="dna">
-                <h1>eDNA</h1>
+            <div className="dna-container">
+                <div className="dna-top">
+                    <div className="dnaButtonContainer">
+                        <button className="startSearchButton">Start Search</button>
+                    </div>
+                </div>
+                <div className="dna-bottom">
+
+                </div>
             </div>
         </>
     );
--- tecxotic/src/Pages/Pilot/Pilot.js
+++ tecxotic/src/Pages/Pilot/Pilot.js
@@ -1,10 +1,12 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import PilotContainer from "../../Components/PilotContainer/PilotContainer";
-import { connectionStatus } from "../../Constants";
-
+import { socket_address } from "../../Constants";
 
 
 export default function PilotPage() {
+
+
+
     const [wifiStatus, setWifiStatus] = useState(true);
     const [gamepadStatus, setGamepadStatus] = useState(true);
     const [flagStatus, setFlagStatus] = useState(false);
@@ -13,9 +15,11 @@ export default function PilotPage() {
     const [pitch, setPitch] = useState(0);
     const [yaw, setYaw] = useState(90);
 
+
+
     return (
         <>
-            <PilotContainer wifiStatus={setWifiStatus} gamepadStatus={setGamepadStatus} flagStatus={setFlagStatus} gearStatus={setGearStatus} rotation={setRotation} pitch={setPitch} yaw={setYaw} />
+            <PilotContainer wifiStatus={wifiStatus} gamepadStatus={gamepadStatus} flagStatus={flagStatus} gearStatus={gearStatus} rotation={rotation} pitch={pitch} yaw={yaw} />
         </>
     );
 }

467a01894f98aa9ee7a912611ceb37eb887f71a8
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-04-09 20:44:29 -0600

    Responsiveness in the ComparatorContainer.

--- tecxotic/src/Components/DNAProp/DNA.js
+++ tecxotic/src/Components/DNAProp/DNA.js
@@ -1,9 +1,16 @@
 import React, { useState } from "react";
 import FileUploader from "../UploadFileButton/UploadFile";
-
+import "./DNA.css"
 
 
 export default function DNA() {
+    const [getDNASampleA, setDNASampleA] = useState("");
+    const [getDNASampleNameA, setDNASampleNameA] = useState("PEZ A");
+    const [getDNASampleB, setDNASampleB] = useState("");
+    const [getDNASampleNameB, setDNASampleNameB] = useState("PEZ B");
+    const [getDNASampleC, setDNASampleC] = useState("");
+    const [getDNASampleNameC, setDNASampleNameC] = useState("PEZ C");
+
     return (
         <>
             <div className="dna-container">
@@ -12,9 +19,78 @@ export default function DNA() {
                         <button className="startSearchButton">Start Search</button>
                     </div>
                 </div>
-                <div className="dna-bottom">
-
+                <div className="dna-bottom-Container">
+                    <div className="DNAContainerFrame">
+                        <div className="dnaAnalysisContainer">
+                            <div className="topAnalysisContainer">
+                                <div className="leftDNAContainer">
+                                    <div className="sampleImgContainer">
+                                        <img className="dnaImage" src={getDNASampleA} alt="DNA Sample A" />
+                                    </div>
+                                </div>
+                                <div className="rightDNAContainer">
+                                    <div className="topMatchedImgContainer">
+                                    </div>
+                                    <div className="bottomSampleMatchedContainer">
+                                        <h1 className="sampleName">{getDNASampleNameA}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="bottomAnalysisContainer">
+                                <div className="fileUploaderContainer">
+                                    <FileUploader onUpload={setDNASampleA} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="DNAContainerFrame">
+                        <div className="dnaAnalysisContainer">
+                            <div className="topAnalysisContainer">
+                                <div className="leftDNAContainer">
+                                    <div className="sampleImgContainer">
+                                        <img className="dnaImage" src={getDNASampleB} alt="DNA Sample B" />
+                                    </div>
+                                </div>
+                                <div className="rightDNAContainer">
+                                    <div className="topMatchedImgContainer">
+                                    </div>
+                                    <div className="bottomSampleMatchedContainer">
+                                        <h1 className="sampleName">{getDNASampleNameB}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="bottomAnalysisContainer">
+                                <div className="fileUploaderContainer">
+                                    <FileUploader onUpload={setDNASampleB} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="DNAContainerFrame">
+                        <div className="dnaAnalysisContainer">
+                            <div className="topAnalysisContainer">
+                                <div className="leftDNAContainer">
+                                    <div className="sampleImgContainer">
+                                        <img className="dnaImage" src={getDNASampleC} alt="DNA Sample C" />
+                                    </div>
+                                </div>
+                                <div className="rightDNAContainer">
+                                    <div className="topMatchedImgContainer">
+                                    </div>
+                                    <div className="bottomSampleMatchedContainer">
+                                        <h1 className="sampleName">{getDNASampleNameC}</h1>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="bottomAnalysisContainer">
+                                <div className="fileUploaderContainer">
+                                    <FileUploader onUpload={setDNASampleC} />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </>
     );

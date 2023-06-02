a75086ef6a860ea3655178df4233057e5ae62198
DulceBucio <114613877+DulceBucio@users.noreply.github.com>
2023-06-01 23:56:17 -0600

    ADD camera and button iterations

--- src/Components/CopilotContainer/CopilotContainer.js
+++ src/Components/CopilotContainer/CopilotContainer.js
@@ -22,22 +22,20 @@ export default function CopilotContainer() {
         </div>
         <div className="right-box">
           <div className="top-right-box">
-            <div className="CameraContainer">
-              <CameraProp
-                image={arrayOfCameras[0].image}
-                idImg={arrayOfCameras[0].idImg}
-                styles={{ width: "95%", height: "95%", borderRadius: "25px" }}
-              />
-            </div>
-          </div>
-          <div className="bottom-right-box">
-            <div className="CameraContainer">
-              <CameraProp
-                image={arrayOfCameras[0].image}
-                idImg={arrayOfCameras[0].idImg}
-                styles={{ width: "95%", height: "95%", borderRadius: "25px" }}
-              />
-            </div>
+            {
+            arrayOfCameras.map((camera, index) => (
+              <div 
+                key={index}
+                className="CameraContainer"
+              >
+                <CameraProp
+                  image={camera.image}
+                  idImg={camera.idImg}
+                  styles={{ width: "95%", height: "95%", borderRadius: "25px" }}
+                />
+              </div>
+            ))
+          }
           </div>
         </div>
       </div>

1b6d669490d7ced7a0a3dd3742c3aaeffbede54d
Ant20ni02 <antoniogarcort@hotmail.com>
2023-06-18 19:35:11 -0600

    Render 3D model finished

--- src/Components/MeasureProp/Measure.js
+++ src/Components/MeasureProp/Measure.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
+import {  useNavigate } from 'react-router-dom';
 import "./Measure.css";
 
 export default function Measure(props) {
+    const navigate = useNavigate();
+
+    const [diameter, setDiameter] = useState('');
+    const [height, setHeight] = useState('');
 
     const takePicture = () => {
         const imageSrc = props.webcam.current.getScreenshot();
@@ -9,6 +14,23 @@ export default function Measure(props) {
         a.href = imageSrc; //Image Base64 Goes here
         a.download = "Measure.png"; //File name Here
         a.click(); 
+    }
+
+    const handleDiameterInput = (e) => {
+        setDiameter(e.target.value); 
+    }
+
+    const handleHeightInput = (e) => {
+        setHeight(e.target.value);
+    }
+
+    const moveTo3D = (e) => {
+        if(diameter !== '' && height !== ''){
+            navigate(`/model/${diameter}/${height}`, { replace: false });
+        }
+        else{
+            alert('You need to fill the two measure inputs');
+        }
     }
 
 
@@ -22,6 +44,22 @@ export default function Measure(props) {
                         Take photo
                     </button>
                 </div>
+                <div className="modelcontainer">
+                    <div className="modelinputcontainer">
+                        <div className="bottomitems">
+                            <input name="diameter" type="number" placeholder={"Diameter"} title="Enter the diameter" onChange={handleDiameterInput} autoComplete="true"></input>
+                        </div>
+                        <div className="bottomitems">
+                            <input name="height" type="number"  placeholder={"Height"} title="Enter the height" onChange={handleHeightInput} autoComplete="true"></input>
+                        </div>
+                    </div>
+                    <div className="bottomitems">
+                        <button className='render3D' onClick={moveTo3D}>
+                            Render 3D
+                        </button>
+                    </div>
+                </div>
+                
             </div>
         </div>
     );

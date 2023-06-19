c17b36212979ccfa2cd0ba72baff4ccda667fc68
Ant20ni02 <antoniogarcort@hotmail.com>
2023-06-18 18:52:43 -0600

    3D Model implemented

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PilotContainer from "./Components/PilotContainer/PilotContainer";
 import PilotPage from './Pages/Pilot/Pilot';
 import CoPilot from './Pages/Copilot/CopilotUI';
+import Model3D from './Pages/Model3D/Model3D';
 
 function scale(number, inMin, inMax, outMin, outMax) {
   return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
@@ -19,6 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<PilotPage/>} />
           <Route path="/copilot" element={<CoPilot/>} />
+          <Route path="/model/:diameter/:height" element={<Model3D/>} />
         </Routes>
       </Router>
       </>
